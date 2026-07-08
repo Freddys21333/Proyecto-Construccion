c8e1af6d3601f4812dfc22049b076a7a2b596ee9
Freddys21333 <freddybajanafuentes@gmail.com>
2026-07-08 14:39:43 -0500

    prueba

---
+++ src/proyecto/foro/de/soporte/tecnico/estudiantil/ProyectoForoDeSoporteTecnicoEstudiantil.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.foro.de.soporte.tecnico.estudiantil;
+
+/**
+ *
+ * @author Carlos.Rezabala
+ */
+public class ProyectoForoDeSoporteTecnicoEstudiantil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
